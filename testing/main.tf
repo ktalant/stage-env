5e31517e1ff0e112f00d52e366b352557d6ecf23
ktalant <uchkunfan@gmail.com>
2020-01-17 15:39:58 -0500

    updated

--- testing/main.tf
+++ testing/main.tf
@@ -7,3 +7,11 @@ resource "aws_default_vpc" "default" {
     Name = "default_vpc"
   }
 }
+
+data "aws_subnet" "sub1" {
+  vpc_id            = aws_default_vpc.default.id 
+}
+
+output "subnet_out" {
+  value = data.aws_subnet.sub1.*.id[0]
+}
